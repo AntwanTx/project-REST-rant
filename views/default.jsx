85655b034c9981f7ce9d7c1120d0097788503c0c
Anthony Munoz <amunoz021@gmail.com>
2023-06-03 02:37:14 -0500

    Adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,14 @@
+const React = require ('react')
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Places</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+module.exports = Def
