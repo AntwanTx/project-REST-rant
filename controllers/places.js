f4f7c14e59922051c853d823aefc583d20d9eaea
Anthony Munoz <amunoz021@gmail.com>
2023-06-03 14:24:39 -0500

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,18 @@
 const router = require('express').Router()
-
+  
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/thai-food.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/kitty-coffee.jpg'
       }]
       
     res.render('places/index',{places})
--- views/error404.jsx
+++ views/error404.jsx
@@ -3,13 +3,29 @@ const Def = require('./default')
 
 function error404 () {
     return (
-        <Def>
-            <main>
-                <h1>404: PAGE NOT FOUND</h1>
-                <p>Oops, sorry, we can't find this page!</p>
-            </main>
-        </Def>
-    )
+      <Def>
+        <main>
+          <h1>404: PAGE NOT FOUND</h1>
+          <p>Oops, sorry, we can't find this page!</p>
+          <div>
+            <img
+              src="/images/sad-pug.jpg"
+              alt="Sad pug puppy wrapped in blanket"
+            />
+            <div>
+              Photo by{" "}
+              <a href="https://unsplash.com/@matthewhenry?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                Matthew Henry
+              </a>{" "}
+              on{" "}
+              <a href="https://unsplash.com/photos/2Ts5HnA67k8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                Unsplash
+              </a>
+            </div>
+          </div>
+        </main>
+      </Def>
+    );
 }
 
 module.exports = error404
