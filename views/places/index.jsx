b6d33fa1ae1741b832b721c5da17311b97d7d218
Anthony Munoz <amunoz021@gmail.com>
2023-06-03 15:41:35 -0500

    updated places page

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,10 +6,17 @@ function index (data) {
     return (
       <div className="col-sm-6">
         <h2>{place.name}</h2>
+        <p className="text-center">
+          {place.cuisines}
+        </p>
         <img src={place.pic} alt={place.name} />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
       </div>
     )
   })
+  
   return (
     <Def>
         <main>
@@ -21,19 +28,5 @@ function index (data) {
     </Def>
   )
 }
-let placesFormatted = data.places.map((place) => {
-  return (
-    <div className="col-sm-6">
-      <h2>{place.name}</h2>
-      <p className="text-center">
-        {place.cuisines}
-      </p>
-      <img src={place.pic} alt={place.name} />
-      <p className="text-center">
-        Located in {place.city}, {place.state}
-      </p>
-    </div>
-  )
-})
 
 module.exports = index
