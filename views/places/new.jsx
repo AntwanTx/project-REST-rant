dd8a76ce51d18e0eab6eb5a2d2a370ad5019e0b9
Anthony Munoz <amunoz021@gmail.com>
2023-06-17 12:18:07 -0500

    added comment form

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,8 +1,15 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form() {
-  return (
+function new_form (data) {
+  let message = ''
+  if(data.message) {
+      message = (
+          <h4 className='alert-danger'>
+              {data.message}
+          </h4>
+      )
+  }  return (
     <Def>
       <main>
         <h1> Add a New Places</h1>
