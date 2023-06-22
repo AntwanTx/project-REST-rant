7e9c2bc30498897708ece8ea42c7b5c9b38fbb27
Anthony Munoz <amunoz021@gmail.com>
2023-06-21 20:15:38 -0500

    Rating is now in stars, and edit now works

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,21 +8,37 @@ function show (data) {
             No comments yet!
         </h3>
     )
-    if (data.place.comments.length) {
-        comments = data.place.comments.map(c => {
-          return (
-            <div className="border">
-              <h2 className="rant">{c.rant ? 'Rant! 🤬': 'Rave! 💖'}</h2>
-              <h4>{c.content}</h4>
-              <h3>
-                <strong>- {c.author}</strong>
-              </h3>
-              <h4>Rating: {c.stars} ⭐️</h4> 
-              {/* <h2 className="recommend">{c.recommend ? 'Recommend! ✅' : 'Do not recommend 🚫'}</h2>  */}
+    let rating = (
+        <h3 className="inactive">
+          Not yet Rated
+        </h3>
+        )
+        if (data.place.comments.length) {
+          comments = data.place.comments.map(c => {
+            return (
+              <div className="border col-sm-4">
+                <h2 className="rant">{c.rant ? 'Rant! 🤬' : 'Rave! 💖'}</h2>
+                <h4>{c.content}</h4>
+                <h3>
+                  <strong>- {c.author}</strong>
+            </h3>
             </div>
+          )})}
+        if (data.place.comments.length) {
+          let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+          }, 0)
+          let averageRating = Math.round(sumRatings / data.place.comments.length)
+          let stars = ''
+          for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+          }
+          rating = (
+            <h3>
+              {stars} stars
+            </h3>
           )
-        })
-      }
+        }
     return (
         <Def>
             <main>
@@ -33,19 +49,19 @@ function show (data) {
                     </div>
                     <div className="col-sm-6">
                         <h1>{data.place.name}</h1>
-                        <h4>Rating</h4>
-                            <h3>Not Rated</h3>
+                        <h2>Rating</h2>
+                            {rating}
                         <br />
                         <h2>Description</h2>
                             <h3>Serving {data.place.cuisines}</h3>
                             <h4>
                                 {data.place.showEstablished()}
                             </h4>
-                        <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
                             Edit
                             </a> 
                              
-                            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
                                 <button type="submit" className="btn btn-danger">
                                     Delete
                                 </button>
@@ -88,3 +104,15 @@ function show (data) {
     )
 }
 module.exports = show
+
+
+
+{/*if (data.place.comments.length) {
+  comments = data.place.comments.map(c => {
+    return (
+      <div className="border col-sm-4">
+        <h2 className="rant">{c.rant ? 'Rant! 🤬' : 'Rave! 💖'}</h2>
+        <h4>{c.content}</h4>
+        <h3>
+          <strong>- {c.author}</strong>
+    </h3>*/}
